73be0736d8d2055189c1d04a5d42eff84427adf7
xqyjjq <walk_code@163.com>
2019-03-04 17:52:03 +0800

    添加項目基础组件

---
+++ src/main/java/com/dodo/project/Application.java
@@ -0,0 +1,23 @@
+package com.dodo.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <b>Application</b></br>
+ *
+ * <pre>
+ * 启动项
+ * </pre>
+ *
+ * @Author xqyjjq walk_code@163.com
+ * @Date 2018/10/6 12:31
+ * @Since JDK 1.8
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
